7e0c2164eb004d427c5486a7491e932752dce65d
David Auza <davidauza01@gmail.com>
2024-10-30 00:59:17 -0500

    build(deployment): update Procfile for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+SIDEKIQ_REDIS_CONFIGURATION = {
+  url: ENV.fetch(ENV.fetch("REDIS_PROVIDER", "REDIS_URL"), nil),
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+}
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = SIDEKIQ_REDIS_CONFIGURATION
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = SIDEKIQ_REDIS_CONFIGURATION
 end
