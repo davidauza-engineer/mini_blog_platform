b7c4c39af1695d9946136e91458366b131858293
David Auza <davidauza01@gmail.com>
2024-10-31 02:43:59 -0500

    feat(security): add string sanitization
    Adding string sanitization before saving any record.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  before_save :sanitize_content
+
+  private
+
+  def sanitize_content
+    self.attributes.each do |key, value|
+      self[key] = ActionController::Base.helpers.sanitize(value) if value.is_a?(String)
+    end
+  end
 end
