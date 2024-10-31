ed3e5cd2bc406496d5e0297d2c1b0ea8db51c9af
David Auza <davidauza01@gmail.com>
2024-10-31 01:00:41 -0500

    feat(security): add CSRF protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
+  protect_from_forgery with: :exception
+
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
