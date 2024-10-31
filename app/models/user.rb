478d504aecd2a6ac8dd8d5b0bbaaa74b0bddb847
David Auza <davidauza01@gmail.com>
2024-10-30 19:14:16 -0500

    feat(RBAC): add role to the users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :comments, foreign_key: :author_id, dependent: :destroy
+
+  enum role: { guest: 'guest', author: 'author' }
 end
